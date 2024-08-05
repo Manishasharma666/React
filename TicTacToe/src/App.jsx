25c9529d3a942ff2883d8b6b1afc12ddcad0cc8b
manisha <manishasharma2431@gmail.com>
2024-08-05 21:02:33 +0530

    created new component

---
+++ TicTacToe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+
+function Square(){
+
+  const [value, setValue] = useState(null);
+
+  function handleClick(){
+      setValue('X');
+  }
+
+    return(
+      <button className="square" onClick={handleClick}>{value}</button>
+    )
+}
+
+
+
+
+
+
+function App() {
+
+  return (
+    <div className="board">
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  )
+}
+
+export default App
